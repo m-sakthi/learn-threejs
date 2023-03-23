f7e96ad126d14adb9a4268ba634a76d7ad97bef1
Sakthivel M <msv300@gmail.com>
2023-03-23 22:02:39 +0100

    Added dat.gui controls

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
 
@@ -15,7 +17,8 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+// controls.addEventListener('change', render) //this line is unnecessary if you are re-rendering within the animation loop
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
@@ -34,6 +37,22 @@ function onWindowResize() {
   render()
 }
 
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+const gui = new GUI()
+const cubeFolder = gui.addFolder('Cube')
+
+const rotation = cube.rotation as Record<string, any>
+cubeFolder.add(rotation, 'x', 0, Math.PI * 2)
+cubeFolder.add(rotation, 'y', 0, Math.PI * 2)
+cubeFolder.add(rotation, 'z', 0, Math.PI * 2)
+cubeFolder.open()
+
+
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position as Record<string, any>, 'z', 0, 20)
+
 function animate() {
   requestAnimationFrame(animate)
 
@@ -41,6 +60,8 @@ function animate() {
   cube.rotation.y += 0.01
 
   render()
+
+  stats.update()
 }
 
 function render() {
@@ -48,3 +69,4 @@ function render() {
 }
 
 animate()
+// render()
